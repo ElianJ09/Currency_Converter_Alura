dc02c7322aea4439870d3bda288f36776929f19d
ElianRG09 <kutakikato80@gmail.com>
2024-07-02 14:11:43 -0600

    CurrencyCalculator Updated

---
+++ README.md
@@ -0,0 +1,23 @@
+# Currency Converter
+
+Alura Latam Challenge
+
+This project was proposed by Alura Latam as part of the Oracle Next Education (ONE) program, aims to apply concepts such as Object-Oriented Programming (OOP), API consumption, JSON library management, and exception handling. It consists of a currency converter with a set number of currencies to choose from, executed from the console. For example, you can convert from US dollars (USD) to Argentine pesos (ARS). The program will provide conversion information and a log history of the conversions made by the user. The Exchange Rate API was used for real-time exchange rates.
+
+
+--- 
+## Menú principal 
+
+Cuenta con 8 opciones de monedas, instrucciones, opción para registro de historial.
+
+![image](https://github.com/kacog/ConversorDeMonedas/assets/110929836/85a49b68-b12b-496c-b6b4-e9884c0260b5)
+
+## Funcionamiento
+
+![image](https://github.com/kacog/ConversorDeMonedas/assets/110929836/959a2d03-45ab-4b63-8a3d-d01768e47151)
+
+## Manejo de excepciones
+![image](https://github.com/kacog/ConversorDeMonedas/assets/110929836/8d1f3a27-7b5d-4074-9133-cb8d439fd753)
+![image](https://github.com/kacog/ConversorDeMonedas/assets/110929836/46114c3e-9bc5-4461-9283-2c4f05c2c7ae)
+
+
--- src/Models/CurrencyCalculator.java
+++ src/Models/CurrencyCalculator.java
@@ -1,4 +1,53 @@
 package Models;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import java.util.Scanner;
+
 public class CurrencyCalculator {
+    Gson listGsonCurrency = new GsonBuilder().setPrettyPrinting().create();
+    Scanner scanner = new Scanner(System.in);
+
+    public double coinsConverter(String coinSelected, String nameCoinSelected, String coinToConverter, String nameCoinToConverter) {
+        String valueClient;
+
+        //Obtains the elements from the json of the API
+        APIConsultor query = new APIConsultor();
+        String value = listGsonCurrency.toJson(query.searchCoinSelected(coinSelected));
+        JsonObject jsonObject = listGsonCurrency.fromJson(value, JsonObject.class);
+
+        //Save the selected coin from the client
+        String coinSelectedClient = jsonObject.get("base_code").getAsString();
+
+        //Value from the coin conversion
+        JsonObject conversionRates= jsonObject.getAsJsonObject("conversion_rates");
+        double valueCoinToConverter = conversionRates.get(coinToConverter).getAsDouble();
+
+        //Obtain value to do a conversion
+        System.out.println("Value from the "+ coinSelectedClient + " to conversion: ");
+        valueClient = scanner.nextLine();
+        valueClient.replace(".",",");
+        Double valueCoverterToDouble = Double.parseDouble(valueClient);
+
+
+        if (valueCoverterToDouble <= 0){
+            System.out.println("The value is minor of 0!. Select a correct value!");
+            valueCoverterToDouble = scanner.nextDouble();
+        } else if (valueCoverterToDouble >= 1){
+            double conversionValue = valueCoverterToDouble * valueCoinToConverter;
+            String conversionValueFromDouble =   String.format("%.2f", conversionValue);
+            System.out.println(valueCoverterToDouble + " " + nameCoinSelected + " " + "====>" + " " + conversionValueFromDouble + " " + nameCoinToConverter);
+
+            System.out.println("""
+            __________________________________________________________________
+            Press zero (0) if you want left.");
+            Press another number [1-8] to continue do convertions);
+            __________________________________________________________________
+            """);
+
+            return conversionValue;
+        }
+        return 0.0; //Just in Case
+    }
 }
